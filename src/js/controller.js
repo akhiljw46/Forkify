c85f146446a1ccad1b0ba106ca0d368af30e2fb2
Akhil Augustin <akhiljw46@gmail.com>
2023-01-28 00:03:35 +0530

    Created a new view for search and added methods to get data from search

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,10 @@
 import * as model from './model.js';
 import recipeView from './view/recipeView.js';
+import searchView from './view/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -13,7 +15,6 @@ const recipeContainer = document.querySelector('.recipe');
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
 
@@ -28,7 +29,24 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    //1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2. Load search results
+    await model.loadSearchResults(query);
+
+    //3. Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
