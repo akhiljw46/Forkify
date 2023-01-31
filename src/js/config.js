33867caed716100afea2348199f02cc097f069b8
Akhil Augustin <akhiljw46@gmail.com>
2023-01-31 23:28:15 +0530

    API key changed

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '8dbbd906-c78e-42de-b175-525ef927ea60';
+// export const KEY = '8dbbd906-c78e-42de-b175-525ef927ea60';
+export const KEY = 'da27028f-40c3-42a9-bee3-7596028c0261';
 export const MODAL_CLOSE_SEC = 1;
